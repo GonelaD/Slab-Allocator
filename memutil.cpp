914319bc6b9e07d2307314b5b7d01c65371fd595
GonelaD <cs16btech11015@iith.ac.in>
2018-04-23 18:33:18 +0530

    added threads to memutil

--- memutil.cpp
+++ memutil.cpp
@@ -1,84 +1,86 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <unistd.h>
 #include "libmymem.hpp"
+
+#define N 10
 
 using namespace std;
 
-typedef struct longdata{
-
-	long int array[1000];
-	struct longdata *ptr=NULL;
-
-} bigData;
-
-int main(){
-	
-	bigData * x[1000];
-
-	cout << sizeof(bigData) << endl;
-
-	x[0] = (bigData *) mymalloc (sizeof(bigData));
-	x[0]->array[0] = 0;
-
-	for ( int i = 1 ; i < 100 ; i++){
-
-
-		x[i] = (bigData *) mymalloc (sizeof(bigData));
-
-		x[i]->array[i] = i;
-		x[i]->ptr = x[i-1];
+void *threadmain(void *niterations);
+int main() {
+ 
+	int nthreads=1;
+	int niterations=5;
+	pthread_t arr[N];
+	pthread_attr_t attr;
+	// Create nthreads to do allocations/free concurrently
+	for (long int t=0; t<nthreads; t++) {
+	// create a list of object sizes for this iththread
 		
-		// if (i == 123) cout << x[i]->array[0] << endl;
-
-		// myfree((void *) x[i]);
+		pthread_attr_init(&attr);
+		int ret=pthread_create(&arr[t], NULL, &threadmain, (void*) niterations);
+		if(ret!=0)
+		{
+			printf("pthread_create failed\n");
+			break;
+		}
+	// Create a thread
 	}
 
-	cout << x[6]->array[0] << endl;
-
-	for (int i = 5 ; i < 13 ; i++ ) {
-		printf("%d\n", i );
-		myfree((void *) x[i]);
-		// printf("Freed %d\n",i);
+	for(long int t=0;t<nthreads;t++){
+		pthread_join(arr[t], NULL);
 	}
+	// wait for all threads to join
+}
+// The thread iterates for niterations and allocates objects of various sizes.
+void *threadmain(void *niterations)
+{
+	//pthread_mutex_t lock;
 
-	for ( int i = 6 ; i < 10 ; i++){
+	int objsizelist[12]={4,8,16,32,64,128,256,512,1024,2048,4096,8192};
+	long int n_iterations =(long int) niterations;
+	srand(time(NULL));
+	printf("itera:%ld\n",n_iterations);
 
-
-		x[i] = (bigData *) mymalloc (sizeof(bigData));
-
-		x[i]->array[i] = i;
-		x[i]->ptr = x[i-1];
+	for (int i=0; i<n_iterations; i++) {
 		
-		// if (i == 123) cout << x[i]->array[0] << endl;
 
-		// myfree((void *) x[i]);
+
+		
+		int objsize = rand()%12;
+		printf("%d th iteration %d\n",i,objsizelist [ objsize ]);
+
+		int *x[5];
+
+		for(int j=0 ; j < 5 ;j++){
+
+			x[j] = (int *)mymalloc (objsizelist [ objsize ] ) ;
+			*x[j] = j;
+		}
+		for(int j=0 ; j < 5 ; j++)
+		{
+			printf("%dth iteration : %d\n",i,*x[j]);
+		}
+
+
+		sleep(5); //sleep for random time
+
+		for(int k=0; k<5; k++){
+
+			myfree((void *)x[i]);
+		}
+		for(int j=0 ; j < 5 ; j++)
+		{
+			printf("%dth iteration : %d\n",i,*x[j]);
+		}
+	//randomly pickup a size from objsizelist;
+	//invoke mymalloc() bail out if error;
+	// write to the allocated memory
+	// sleep for small random time (no sleep causes more contention)
+	// call myfree()
 	}
-
-	// for (int i = 5 ; i < 13 ; i++ ) {
-	// 	printf("%d\n", i );
-	// 	myfree((void *) x[i]);
-	// }
-
-	// printf("%ld %ld\n", x[5], x[6]);
-
-
-	// myfree((void *)x[5]);
-
-	// int *y;
-	// y = (int *) mymalloc(sizeof(int));
-
-	// *y = 28;
-
-	// int *x;
-	// x = (int *) mymalloc(sizeof(int));
-
-	// *x = 38;
-
-	// myfree(y);
-	// myfree(x);
-
-	// printf("%d\n", *y);
-
-	// printf("%d\n",sizeof(int));
-
-	return 0;
+	pthread_exit(0);
 }
